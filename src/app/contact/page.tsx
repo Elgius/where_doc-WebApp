d21374c36222e04e7ce8a6780137267f3616f6a8
Elgius <114131653+Elgius@users.noreply.github.com>
2024-02-24 13:57:51 +0500

    we got this

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils";
 
 export default function Page() {
   return (
+    // Bug: animations not smooth in dev mode, check prod server to verify if issue persists
     <div className="h-screen bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
       <div className="max-w-2xl mx-auto p-4">
         <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
