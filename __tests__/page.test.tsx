db2a1131e973bfa352dc9f43a6b9b7d6f744233c
Arghya <bhowmickarghyadeep2@gmail.com>
2024-03-23 12:46:20 +0530

    Unit testing setup completed

---
+++ __tests__/page.test.tsx
@@ -0,0 +1,15 @@
+// Sample test
+
+// import '@testing-library/jest-dom'
+// import { render, screen } from '@testing-library/react'
+// import Home from '@/app/page'
+
+// describe('Home', () => {
+//   it('renders a heading', () => {
+//     render(<Home />)
+
+//     const heading = screen.getByRole('heading', { level: 1 })
+
+//     expect(heading).toBeInTheDocument()
+//   })
+// })
