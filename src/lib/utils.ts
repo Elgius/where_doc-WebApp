d6185519a2310ea78eb2fb16a2935dd0286008e2
Nauf <130215373+naufudge@users.noreply.github.com>
2024-04-27 23:46:09 +0500

    added function to convert doctor names, to url route (remove spaces and punctuation)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function convertNametoURLParam(params: String) {
+  // Converts Doctor's name into URL route.
+  // For example: Dr. Jane Smith -> dr-jane-smith
+  const newParam = params.replaceAll(" ", "-").replaceAll(".", "").toLowerCase();
+  return newParam;
+}
